00ba990dd32c0853a87d068cb41ce57c622a9d19
namazso <admin@namazso.eu>
2025-06-24 06:03:16 +0200

    Delete ThemeTool, ThemeLib, ThemeDll

--- bin2h.ps1
+++
@@ -1,8 +0,0 @@
-param(
-    [Parameter(Mandatory, Position = 0)]
-    [string]$InputFile,
-    [Parameter(Mandatory, Position = 1)]
-    [string]$OutputFile
-)
-
-((Get-Content -Encoding byte $InputFile | % { [int32]$_ }) -join ",") >$OutputFile
